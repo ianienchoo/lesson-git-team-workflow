9593a84323885b782bf1aa63083114de17c62991
ianchoo <ianienchoo@gmail.com>
2015-11-09 13:46:55 +0800

    messed up controllers

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,27 @@
 class ActorsController < ApplicationController
 
+def index
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+private
+
+def actors_params
+  params.require(:actors).permit(:name, :year_of_birth, :thumbnail)
+end
+
 end
